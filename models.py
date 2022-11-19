ca5a75c4e5b92618d0032c913e2309b27bd8d45c
Om Bhoge <114462074+OM28-bhoge@users.noreply.github.com>
2022-11-19 21:44:50 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Student(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='',null=True,blank=True)
-    address = models.CharField(max_length=40)
-    mobile = models.CharField(max_length=20,null=False)
